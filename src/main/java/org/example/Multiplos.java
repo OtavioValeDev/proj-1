7ff9e2dcebc73536b24ca7e7891cbfaea2d0707c
OtavioValeDev <otavioaugusto_2007@outlook.com>
2025-08-30 15:37:07 -0300

    exercicio de multiplos

---
+++ src/main/java/org/example/Multiplos.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Multiplos {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.printf("FizzBuzz");
+
+            } else if (i % 3 == 0) {
+                System.out.printf("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.printf("Buzz");
+            } else {
+                System.out.printf("i");
+            }
+
+        }
+    }
+}
